2569345424f1ce4796e0aac535f82a7b6c5de62a
toshi-toma <toshitoma1217@gmail.com>
2019-03-17 22:34:17 +0900

    Save the switch value and textarea value

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,11 +1,21 @@
 'use strict';
 
-const checkbox = document.querySelector('body > label > input[type="checkbox"]')
+// isExecut checkbox
+const checkbox = document.querySelector('body > label > input[type="checkbox"]');
+chrome.storage.local.get(['isExecute'], result => {
+  checkbox.checked = result.isExecute;
+});
 checkbox.addEventListener('change', e => {
-  alert(e.target.checked);
+  const isExecute = e.target.checked;
+  chrome.storage.local.set({isExecute}, () => {});
 });
 
+// textarea for js code and save button
+const textarea = document.querySelector('body > textarea');
 const saveButton = document.querySelector('body > button');
-saveButton.addEventListener('click', e => {
-  alert(e);
+chrome.storage.local.get(['jsCode'], result => {
+  textarea.value = result.jsCode;
+});
+saveButton.addEventListener('click', () => {
+  chrome.storage.local.set({ 'jsCode': textarea.value }, () => {});
 });
