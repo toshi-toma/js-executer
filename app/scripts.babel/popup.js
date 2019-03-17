c1048d7be897bdc19b326b2850d3425b5b81a51c
toshi-toma <toshitoma1217@gmail.com>
2019-03-17 21:46:44 +0900

    add EventListener for button and checkbox

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,3 +1,11 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Popup');
+const checkbox = document.querySelector('body > label > input[type="checkbox"]')
+checkbox.addEventListener('change', e => {
+  alert(e.target.checked);
+});
+
+const saveButton = document.querySelector('body > button');
+saveButton.addEventListener('click', e => {
+  alert(e);
+});
